c379435e9f7de56b28346e1be1e299606d8e009f
Robert Morain <rmorain2@login01.rc.byu.edu>
2023-09-26 16:23:35 -0600

    Changed gitignore to allow WandB files

---
+++ gpt2-imdb-prompt-generation-7-emotions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --time=10:00:00   # walltime
+#SBATCH --ntasks=16   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=1024M   # memory per CPU core
+#SBATCH -J "test-gst"   # job name
+#SBATCH --mail-user=rmorain2@byu.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --qos=cs
+
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+export WANDB_API_KEY=e279feeab3d602ab530e4eb23df8ac3ff3763461
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+mamba activate sentiment-tuning
+wandb login --relogin e279feeab3d602ab530e4eb23df8ac3ff3763461
+python general_sentiment_tuning.py
