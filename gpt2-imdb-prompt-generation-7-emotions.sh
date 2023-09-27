d4b85f482f0d8f7b1eb52622bf622c2460c0595b
Robert Morain <rmorain2@login01.rc.byu.edu>
2023-09-27 16:10:29 -0600

    Added script

--- gpt2-imdb-prompt-generation-7-emotions.sh
+++ gpt2-imdb-prompt-generation-7-emotions.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gpus=1
 #SBATCH --mem-per-cpu=1024M   # memory per CPU core
-#SBATCH -J "test-gst"   # job name
+#SBATCH -J "gpt2-imdb-prompt-generation-7-emotions"   # job name
 #SBATCH --mail-user=rmorain2@byu.edu   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -16,6 +16,8 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 export WANDB_API_KEY=e279feeab3d602ab530e4eb23df8ac3ff3763461
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+wandb offline
 mamba activate sentiment-tuning
 wandb login --relogin e279feeab3d602ab530e4eb23df8ac3ff3763461
 python general_sentiment_tuning.py
+
