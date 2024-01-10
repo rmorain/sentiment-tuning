09721f5694705ed9060e305bf047814800ebd02c
Robert Morain <robert.morain@gmail.com>
2024-01-10 09:19:42 -0700

    Added more memory

--- ppo_toy_problem.sh
+++ ppo_toy_problem.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=64G   # memory per CPU core
+#SBATCH --mem-per-cpu=256G   # memory per CPU core
 #SBATCH -J "debug: ppo toy_problem"   # job name
 #SBATCH --mail-user=rmorain2@byu.edu   # email address
 #SBATCH --mail-type=BEGIN
