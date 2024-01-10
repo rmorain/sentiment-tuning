8f54267dd9e3e0bb0fde41e3f7a99c3c2318f664
Robert Morain <robert.morain@gmail.com>
2024-01-10 09:19:29 -0700

    Fixed errors in computing rewards

--- ppo_toy_problem.py
+++ ppo_toy_problem.py
@@ -1,3 +1,4 @@
+import time
 from random import choice
 
 import numpy as np
@@ -23,14 +24,18 @@ def compute_reward(responses, target, reward_model, device):
     """
     rewards = []
     predictions = []
+    accuracies = []
     with torch.no_grad():
         for i, response in enumerate(responses):
             labels = torch.full(response.shape, -100)
             labels[-len(target[i]) :] = target[i]
             output = reward_model(response, labels=labels.to(device))
             rewards.append(-output.loss)
-            predictions.append(output.logits[-1].argmax())
-    return rewards, predictions
+            prediction = output.logits[-1].argmax()
+            predictions.append(prediction)
+            accuracies.append((prediction == labels[-1]).item())
+        mean_accuracy = np.mean(accuracies)
+    return rewards, predictions, mean_accuracy
 
 
 class ColorDataset(Dataset):
@@ -42,13 +47,14 @@ class ColorDataset(Dataset):
         return self.batch_size
 
     def __getitem__(self, *args):
-        target = choice(["red", "blue"])
+        target = choice([" Red", " Blue"])
         prompt = " What is the color of the book?"
-        target_ids = self.tokenizer("The book is " + target + ".", return_tensors="pt")[
-            "input_ids"
-        ].squeeze(0)
+        target_ids = self.tokenizer(
+            "The book is " + target.strip().lower() + ".", return_tensors="pt"
+        )["input_ids"].squeeze(0)
         prompt_ids = self.tokenizer(prompt, return_tensors="pt")["input_ids"].squeeze(0)
-        return {"target": target_ids, "prompt": prompt_ids}
+        target = self.tokenizer(target, return_tensors="pt")["input_ids"].squeeze(0)
+        return {"target": target, "target_ids": target_ids, "prompt": prompt_ids}
 
 
 def clean_stats(stats):
@@ -58,18 +64,30 @@ def clean_stats(stats):
     return stats
 
 
+def get_response_masks(prefix_tensors, response_tensors, device):
+    response_masks = []
+    for p, r in zip(prefix_tensors, response_tensors):
+        prefix_mask = torch.ones(len(p), dtype=torch.long)
+        r_mask = torch.zeros(len(r) - len(p), dtype=torch.long)
+        response_mask = torch.cat((prefix_mask, r_mask), dim=0)
+        response_masks.append(response_mask)
+    return response_masks
+
+
 def main():
     torch.manual_seed(0)
-    run = wandb.init(project="debugging-ppo", name="debug")
+    run = wandb.init(project="debugging-ppo")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     config = PPOConfig(
         model_name="gpt2",
         learning_rate=1.41e-5,
-        batch_size=128,
+        batch_size=256,
         log_with="wandb",
-        ratio_threshold=100,
-        use_score_scaling=True,
-        whiten_rewards=True,
+        ratio_threshold=10,
+        # use_score_scaling=True,
+        # whiten_rewards=True,
+        kl_penalty="abs",
+        mini_batch_size=64,
     )
 
     # Load pretrained models
@@ -107,12 +125,22 @@ def main():
     debounce = 0
     debounce_limit = 20
     debug = False
-    bssf_table = wandb.Table(columns=["Prefix", "Reward", "Prediction"])
-    max_steps = 1000
+    bssf_table = wandb.Table(
+        columns=[
+            "Prefix",
+            "Reward",
+            "Prediction",
+            "Prediction Token",
+            "Target",
+            "Target Token",
+        ]
+    )
+    max_steps = 100
     step = 0
     while not done:
         for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
-            target_tensors = batch["target"]
+            target_tensors = batch["target_ids"]
+            targets = batch["target"]
             prompt_tensors = batch["prompt"]
             query_tensors = [
                 torch.cat((target_tensors[i], prompt_tensors[i]))
@@ -120,12 +148,7 @@ def main():
             ]
             batch["query"] = [tokenizer.decode(q.squeeze()) for q in query_tensors]
 
-            try:
-                generated_tokens, scores = ppo_trainer.generate(
-                    query_tensors, **gen_kwargs
-                )
-            except RuntimeError:
-                pu.db
+            generated_tokens = ppo_trainer.generate(query_tensors, **gen_kwargs)
             prefix_tensors = [
                 generated_tokens[i][len(query_tensors[i]) :]
                 for i in range(len(generated_tokens))
@@ -138,18 +161,33 @@ def main():
             batch["response"] = [
                 tokenizer.decode(r.squeeze()) for r in response_tensors
             ]
-            rewards, predictions = compute_reward(
-                response_tensors, batch["target"], reward_model, device
+            rewards, predictions, mean_accuracy = compute_reward(
+                response_tensors, targets, reward_model, device
             )
 
             # Run PPO step
-            stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
+            response_masks = get_response_masks(
+                prefix_tensors, response_tensors, device
+            )
+            stats = ppo_trainer.step(
+                query_tensors, response_tensors, rewards, response_masks
+            )
             best_reward_index = torch.tensor(rewards).argmax()
             mean_reward = torch.tensor(rewards).mean()
             best_prefix = tokenizer.decode(prefix_tensors[best_reward_index])
             best_reward = rewards[best_reward_index]
             prediction = tokenizer.decode(predictions[best_reward_index])
-            bssf_table.add_data(best_prefix, best_reward, prediction)
+            prediction_token = predictions[best_reward_index]
+            target_token = targets[best_reward_index]
+            target = tokenizer.decode(target_token)
+            bssf_table.add_data(
+                best_prefix,
+                best_reward,
+                prediction,
+                prediction_token,
+                target,
+                target_token,
+            )
             # Filter stats for infinity
             stats = clean_stats(stats)
             ppo_trainer.log_stats(
@@ -158,30 +196,32 @@ def main():
                 rewards,
                 columns_to_log=["query", "response", "prefix"],
             )
+            run.log({"mean_accuracy": mean_accuracy})
 
             # Check if done
+            step += 1
             if debug:
                 done = True
                 continue
-            if max_steps is not None and step < max_steps:
-                step += 1
-                done = False
-                continue
-            if best_reward > best_reward_so_far:
-                best_reward_so_far = best_reward
-                debounce = 0
-            elif mean_reward > prev_reward_mean:
-                prev_reward_mean = mean_reward
-                debounce = 0
-            elif debounce > debounce_limit:
+            if max_steps is not None and step >= max_steps:
                 done = True
-            else:
-                debounce += 1
+                continue
+            # if best_reward > best_reward_so_far:
+            #     best_reward_so_far = best_reward
+            #     debounce = 0
+            # elif mean_reward > prev_reward_mean:
+            #     prev_reward_mean = mean_reward
+            #     debounce = 0
+            # elif debounce > debounce_limit:
+            #     done = True
+            # else:
+            #     debounce += 1
 
     # Save model
     run.log({"BSSF Table": bssf_table})
-    model.save_pretrained("saved_models/ppo_color_model.pt")
-    tokenizer.save_pretrained("saved_models/ppo_color_model_tokenizer.pt")
+    time_str = time.strftime("%Y%m%d-%H%M%S")
+    model.save_pretrained(f"saved_models/ppo_color_model_{time_str}.pt")
+    tokenizer.save_pretrained(f"saved_models/ppo_color_model_tokenizer_{time_str}.pt")
 
 
 if __name__ == "__main__":
